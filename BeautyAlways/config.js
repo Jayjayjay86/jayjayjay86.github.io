17b384a812104a2ec29c54836af016e6abd18310
John <112680154+Jayjayjay86@users.noreply.github.com>
2026-08-19 21:20:45 +0700

    Update config.js

--- BeautyAlways/config.js
+++ BeautyAlways/config.js
@@ -2,10 +2,12 @@
 // This file holds configuration that would normally be in .env
 // For security, you should exclude this from version control in production
 
-const CONFIG = {
-  CHANNEL3_API_KEY: 'MbvJg8UkmC6A3knEYO2Vx8dOV2yZWb5Y5lVQw5zP',
-  CHANNEL3_API_URL: 'https://api.trychannel3.com/v1/search'
-};
+const CHANNEL3_API_KEY = 'MbvJg8UkmC6A3knEYO2Vx8dOV2yZWb5Y5lVQw5zP';
+const CHANNEL3_API_URL = 'https://api.trychannel3.com/v1/search';
+const MAKEUP_API_URL = 'https://makeup-api.herokuapp.com/api/v1/products.json';
 
+// CORS Proxy - use this to bypass CORS restrictions
+// You can also use: https://api.allorigins.win/raw?url=
+const CORS_PROXY = 'https://corsproxy.io/?';
 // Make it available globally
 window.CONFIG = CONFIG;
